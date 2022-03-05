2a9534337db13757ea90d16964eab829f7692343
bigBear713 <774326739@qq.com>
2022-03-05 15:56:10 +0800

    UPDATE: demo project

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ng-common'),
+      dir: require('path').join(__dirname, './coverage/ng-common-demo'),
       subdir: '.',
       reporters: [
         { type: 'html' },
