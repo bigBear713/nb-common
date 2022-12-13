ff5eaa2e21bba9c1c355e87fd393988d6f7754d3
bigBear713 <774326739@qq.com>
2022-12-13 15:40:46 +0800

    ADD: demo for used in standalone components

---
+++ src/app/standalone-demo/standalone-demo-routing.ts
@@ -0,0 +1,5 @@
+import { StandaloneDemoComponent } from "./standalone-demo.component";
+
+export const standaloneRoutes = [
+    { path: '', component: StandaloneDemoComponent }
+];
---
+++ src/app/standalone-demo/standalone-demo.component.ts
@@ -0,0 +1,52 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { NbCommonModule, NB_DEFAULT_LOADING_IMG } from 'nb-common';
+import { BehaviorSubject } from 'rxjs';
+
+@Component({
+  standalone: true,
+  selector: 'app-standalone-demo',
+  templateUrl: './standalone-demo.component.html',
+  styleUrls: ['./standalone-demo.component.css'],
+  imports: [CommonModule, NbCommonModule],
+  providers: [
+    {
+      provide: NB_DEFAULT_LOADING_IMG,
+      useFactory: (domSanitizer: DomSanitizer) => {
+        return domSanitizer.bypassSecurityTrustResourceUrl('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIiBzdHlsZT0ibWFyZ2luOmF1dG87YmFja2dyb3VuZDp0cmFuc3BhcmVudDtkaXNwbGF5OmJsb2NrOyIgd2lkdGg9IjIwMHB4IiBoZWlnaHQ9IjIwMHB4IiB2aWV3Qm94PSIwIDAgMTAwIDEwMCIgcHJlc2VydmVBc3BlY3RSYXRpbz0ieE1pZFlNaWQiPgo8cGF0aCBkPSJNMTcgNTBBMzMgMzMgMCAwIDAgODMgNTBBMzMgMzUgMCAwIDEgMTcgNTAiIGZpbGw9IiM0MGE5ZmYiIHN0cm9rZT0ibm9uZSI+CiAgPGFuaW1hdGVUcmFuc2Zvcm0gYXR0cmlidXRlTmFtZT0idHJhbnNmb3JtIiB0eXBlPSJyb3RhdGUiIGR1cj0iMXMiIHJlcGVhdENvdW50PSJpbmRlZmluaXRlIiBrZXlUaW1lcz0iMDsxIiB2YWx1ZXM9IjAgNTAgNTE7MzYwIDUwIDUxIj48L2FuaW1hdGVUcmFuc2Zvcm0+CjwvcGF0aD4KPC9zdmc+')
+      },
+      deps: [DomSanitizer]
+    }
+  ]
+})
+export class StandaloneDemoComponent implements OnInit {
+
+  promiseDemo = new Promise<string>(
+    resolve => setTimeout(() => resolve('this is an async string value '), 1000)
+  );
+
+  bigImg = 'https://pic2.zhimg.com/v2-77be54b348b0168f3562f51404451aeb_r.jpg';
+
+  placeholder$ = new BehaviorSubject('这是placeholder');
+
+  intervalId!: any;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.intervalUpdatePlaceholderStr();
+  }
+
+  ngOnDestroy(){
+    clearInterval(this.intervalId);
+  }
+
+  private intervalUpdatePlaceholderStr(): void {
+    this.intervalId = setInterval(() => {
+      const str = this.placeholder$.value === '这是placeholder' ? 'This is placeholder' : '这是placeholder';
+      this.placeholder$.next(str);
+    },3000);
+  }
+
+}
