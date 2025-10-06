a491c2327e3a6add58dc178706650326ece51059
bigBear713 <774326739@qq.com>
2025-10-06 22:52:47 +0800

    chore: update project configuration

---
+++ src/app/app.ts
@@ -0,0 +1,34 @@
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { GTagService } from './g-tag.service';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class App {
+  private gtagService:GTagService = inject(GTagService);
+
+  protected readonly title = signal('nb-common-demo');
+
+  links = [
+    {
+      title: 'Document',
+      link: 'https://github.com/bigBear713/nb-common/blob/main/projects/nb-common/README.md',
+    },
+    {
+      title: 'Changelog',
+      link: 'https://github.com/bigBear713/nb-common/blob/main/CHANGELOG.md',
+    },
+  ];
+
+  go2Link(target: { title: string; link: string }): void {
+    this.gtagService.trackLink({
+      link_name: target.title,
+      link: target.link,
+    });
+  }
+}
